291ea53050d0cf6c4f745200932844654a1c3299
unknown <david.keller.fr@gmail.com>
2020-09-07 18:51:00 +0200

    WIP: upload files in mail

--- mail-api/src/main/java/drkeller/mail/mailapi/controller/MultipartDocumentController.java
+++ mail-api/src/main/java/drkeller/mail/mailapi/controller/MultipartDocumentController.java
@@ -4,33 +4,22 @@ import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 import static org.springframework.http.ResponseEntity.ok;
 
-import java.io.InputStream;
 import java.util.Map;
-import java.util.concurrent.Callable;
 
-import org.bson.types.ObjectId;
-import org.reactivestreams.Publisher;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.ReactiveGridFsTemplate;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ServerWebExchange;
 
 import lombok.RequiredArgsConstructor;
@@ -40,61 +29,84 @@ import reactor.core.publisher.Mono;
 
 @CrossOrigin("*")
 @RestController()
-@RequestMapping("/api/document/multipart")
+@RequestMapping("/api/mail/document")
 @RequiredArgsConstructor
 public class MultipartDocumentController {
 	
 	@Autowired
 	private final ReactiveGridFsTemplate gridFsTemplate = null;
 
-	@PostMapping("noreactive")
-	public Mono<ResponseEntity> uploadFile(@RequestParam("file") MultipartFile file) {
-		int bufferSize = 1;
-		Callable<InputStream> inputStreamSupplier = new Callable<InputStream>() {
-			@Override
-			public InputStream call() throws Exception {
-				return file.getInputStream();
-			}
-		}; 
-		
-		Publisher<DataBuffer> p = DataBufferUtils.readInputStream(inputStreamSupplier, new DefaultDataBufferFactory(), bufferSize);
-		System.out.println("p: " + p);
-		System.out.println("gridFsTemplate: " + gridFsTemplate);
-		Mono<ObjectId> monoId = this.gridFsTemplate.store(p, file.getName());
-		System.out.println("monoId = " + monoId);
-		return monoId.map((id) -> ok().body(Map.of("id", id.toHexString())));
-	}
+	@RequestMapping(value = "{mailId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, method = RequestMethod.POST) 
+    public Mono<Object> upload(
+    		@PathVariable String mailId, 
+    		@RequestPart("file") Mono<FilePart> file, 
+    		@RequestPart("metadata") String metadataString) {
 
-	
-	@RequestMapping(value = "reactive", produces = MediaType.APPLICATION_JSON_VALUE, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, method = RequestMethod.POST) 
-    public Mono<ResponseEntity> upload(@RequestPart("file") Mono<FilePart> file) {
-        return file
+		return file
         	.log()
-            .flatMap(part -> this.gridFsTemplate.store(part.content(), part.filename()))
+            .flatMap(part -> {
+        		Document md = null;
+        		if (metadataString == null) {
+        			md = new Document();
+        		} else {
+        			md = Document.parse(metadataString);
+        		}
+    			md.put("mailId", mailId);
+            	return this.gridFsTemplate.store(part.content(), part.filename(), md);	
+            })
             .map((id) -> ok().body(Map.of("id", id.toHexString())));
     }
 
 
-    @GetMapping("{id}")
-    public Flux<Void> read(@PathVariable String id, ServerWebExchange exchange) {
+    @GetMapping("{mailId}/{id}/raw")
+    public Flux<Void> read(
+    		@PathVariable String mailId, 
+    		@PathVariable String id, 
+    		ServerWebExchange exchange) {
+    	HttpHeaders responseHeaders = exchange.getResponse().getHeaders();
+    	
         return this.gridFsTemplate.findOne(query(where("_id").is(id)))
-            .log()
             .flatMap(gridFsTemplate::getResource)
-            .flatMapMany(r -> exchange.getResponse().writeWith(r.getDownloadStream()));
+            .flatMapMany(r -> {
+            	responseHeaders.set("Content-Disposition", "attachment; filename=\"" + r.getFilename() + "\"");
+            	return exchange.getResponse().writeWith(r.getDownloadStream());
+            });
     }
+
+    @GetMapping("{mailId}/{id}/metadata")
+    public Mono<Document> getMetaData(
+    		@PathVariable String mailId, 
+    		@PathVariable String id) {
+    	return this.gridFsTemplate.findOne(query(where("_id").is(id)))
+	        .log()
+            .map(r -> {
+            	return r.getMetadata();
+            });
+    }
+
+	@RequestMapping(value = "{mailId}/{id}/metadata", produces = MediaType.APPLICATION_JSON_VALUE, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, method = RequestMethod.POST) 
+//    @PostMapping("{mailId}/{id}/metadata")
+    public Mono<Document> setMetaData(
+    		@PathVariable String mailId, 
+    		@PathVariable String id, 
+    		@RequestBody Map<String,Object> metadata) {
+    	return this.gridFsTemplate.findOne(query(where("_id").is(id)))
+	        .log()
+            .map(r -> {
+            	r.getMetadata().putAll(metadata);
+            	return r.getMetadata();
+            });
+    }
+
     
-    
-//    @GetMapping("/")
-//    public Mono<ResponseEntity> getFiles() {
-//    	Query query = new Query();
-//    	this.gridFsTemplate.find(query)
-////    		.flatMap(doc -> { return doc.getId(); }).then()
-//    		.map((id) -> ok().body(Map.of("id", id.toHexString())));
-//    	
-////        return file
-////        	.log()
-////            .flatMap(part -> this.gridFsTemplate.store(part.content(), part.filename()))
-////            .map((id) -> ok().body(Map.of("id", id.toHexString())));
-//    }
+    @GetMapping("{mailId}")
+    public Flux<Document> getFiles(@PathVariable String mailId) {
+    	return this.gridFsTemplate.find(query(where("mailId").is(mailId)))
+    	        .log()
+                .map(r -> {
+                	return r.getMetadata();
+                });
+    	
+    }
 
 }
